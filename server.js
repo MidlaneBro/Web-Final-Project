76742b7bea876a94e5f9a31b489564e03c6e993b
MidLaneBro <chung.a40709@gmail.com>
2021-01-20 18:25:34 +0800

    uppercase

--- server.js
+++ server.js
@@ -515,8 +515,8 @@ db.once('open', () => {
       })
   })
 
-  const PORT = process.env.port || 3000
-  server.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`)
+  const port = process.env.PORT || 3000
+  server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
   })
 });
